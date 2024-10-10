12723a09db741e9a1ce32623d526b1f1c08067da
wandbergdelima <wandberg@gmail.com>
2024-10-10 14:32:27 -0300

    feat: first commit

---
+++ src/main/java/modelo/full/api/BackEndModeloApplication.java
@@ -0,0 +1,13 @@
+package modelo.full.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndModeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndModeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/modelo/full/api/BackEndModeloApplicationTests.java
@@ -0,0 +1,13 @@
+package modelo.full.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndModeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
